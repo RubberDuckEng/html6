3d645a2fcc5688b9ff570ed73fb8e1cf3b4af2fd
Eric Seidel <github@eseidel.com>
2022-03-26 13:43:59 -0700

    Fix entity lookup and pass 1.4k more tests
    Our entity matching is still not 100% correct, but this at least fixes the bug where it was forgetting which entity range had already matched the prefix by not updating firstPossible, meaning it would return the first entity which matched a character at the end of the entity.
    
    This still has a bug of not matching the longest possible entity so many tests have an extra ";" as a result.

--- lib/src/entities.dart
+++ lib/src/entities.dart
@@ -10,8 +10,11 @@ class Entity {
 }
 
 Entity? findMatchingEntity(InputManager input, StringBuffer consumed) {
+  // firstPossible/oneAfterLastPossible limit our search window to
+  // entities whose prefix we've already matched.
   var firstPossible = 0;
   var oneAfterLastPossible = entities.length;
+
   for (int codeOffset = 0; codeOffset < maxEntityLength; codeOffset++) {
     // 1 accounts for the amperstand.
     var offsetInEntity = 1 + codeOffset;
@@ -22,10 +25,15 @@ Entity? findMatchingEntity(InputManager input, StringBuffer consumed) {
     var actual = input.getNextCodePoint();
     consumed.writeCharCode(actual);
     int entityIndex = firstPossible;
+    // Check all possible entities for a match of current 'actual' char.
     while (entityIndex < oneAfterLastPossible) {
       var entity = entities[entityIndex];
       if (offsetInEntity < entity.nameCodepoints.length &&
           entity.nameCodepoints[offsetInEntity] == actual) {
+        if (!foundMatch) {
+          // Ignore any entities before this one as they don't prefix match.
+          firstPossible = entityIndex;
+        }
         foundMatch = true;
         // We found matching entity;
         if (offsetInEntity == entity.nameCodepoints.length - 1) {
@@ -36,17 +44,18 @@ Entity? findMatchingEntity(InputManager input, StringBuffer consumed) {
       } else {
         entityIndex += 1;
         if (foundMatch) {
+          // Ignore entities after, they've started failing to match prefix.
           oneAfterLastPossible = entityIndex;
           break;
         }
         // Still haven't found first match, keep looking.
       }
-      if (oneAfterLastPossible - firstPossible <= 1) {
-        // Ran out of entities to possibly match.
-        return null;
-      }
     }
-    // No match, give up.
+    if (oneAfterLastPossible - firstPossible <= 1) {
+      // Ran out of entities to possibly match.
+      return null;
+    }
+    // Still have entities matching this prefix, try the next letter.
   }
   return null;
 }
